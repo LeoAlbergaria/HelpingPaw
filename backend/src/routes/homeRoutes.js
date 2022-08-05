2c5b16e3d4ba4874cb8481a8ba340e63a797abe9
Christian Junji <71985188+junji-code@users.noreply.github.com>
2022-08-05 19:01:59 -0300

    Delete homeRoutes.js

--- backend/src/routes/homeRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const homeController = require('../controllers/HomeController');
-
-const router = new Router();
-
-router.get('/', homeController.index);
-
-module.exports = router;
